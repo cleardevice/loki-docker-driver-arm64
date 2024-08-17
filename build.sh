4481139ff2252e6d796faa4465d56a430ed129a6
cd <cleardevice@gmail.com>
2024-08-17 14:21:05 +0000

    Update build.sh, use buildx

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 LOKI_DRIVER_VERSION=$1
 
-docker build -t rootfsimage . --build-arg LOKI_DRIVER_VERSION=$LOKI_DRIVER_VERSION
+docker buildx build -t rootfsimage . --build-arg LOKI_DRIVER_VERSION=$LOKI_DRIVER_VERSION --platform linux/arm64
 id=$(docker create rootfsimage true)
 mkdir -p plugin/rootfs
 docker export "$id" | tar -x -C plugin/rootfs
