e49d8ee0535e2e475bdc98819472901cd7fdd7b1
cd <cleardevice@gmail.com>
2024-08-17 13:02:46 +0000

    Fix push.sh, add LOKI_DRIVER_VERSION variable, remove sudo, rename workflow

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
+LOKI_DRIVER_VERSION=$1
+
 docker plugin create cleardevice/loki-docker-driver-arm64:$LOKI_DRIVER_VERSION plugin/
 docker plugin push cleardevice/loki-docker-driver-arm64:$LOKI_DRIVER_VERSION
 docker plugin rm cleardevice/loki-docker-driver-arm64:$LOKI_DRIVER_VERSION
